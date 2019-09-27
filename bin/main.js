2f5e6786da9acfa0883953ea443c37945353e659
Cezary Grzanka <cezarygrzanka@gmail.com>
2019-09-27 23:57:28 +0200

    Prettier works!

--- bin/main.js
+++ bin/main.js
@@ -6,16 +6,14 @@ const minimist = require('minimist');
 const args = minimist(process.argv.slice(2), {
   alias: {
     p: 'prepare',
-    h: 'help'
-  }
+    h: 'help',
+  },
 });
 
 if (args.h || args.h) {
   console.log(
-    '-h, --help: Display this screen\n-p, --prepare: Prepare directory with my standarts setup\n'
+    '-h, --help: Display this screen\n-p, --prepare: Prepare directory with my standarts setup\n',
   );
 }
 
-if (args.hasOwnProperty('p') || args.hasOwnProperty('prepare')) {
-  console.log(prepare());
-}
+if (args.hasOwnProperty('p') || args.hasOwnProperty('prepare')) prepare();
